a440c7fb0087d72c36e1d0f716b2821e34c408da
TTVpetergriffin <112569065+TTVpetergriffin@users.noreply.github.com>
2022-09-16 12:27:19 -0400

    ok

--- Prototype 2 - Animals/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2 - Animals/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private GameObject dog;
+    bool cd = false;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
+            if (dog == null)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dog = Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                if (dog != null) cd = true;
         }
     }
 }
