859d95728fc2ae73f7dc9dcc5a0807a5b29fc7ac
TTVpetergriffin <112569065+TTVpetergriffin@users.noreply.github.com>
2022-10-26 12:27:45 -0400

    continued work on performance test 2

--- Performance tests/Assets/Scripts/playerController.cs
+++ Performance tests/Assets/Scripts/playerController.cs
@@ -2,21 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float xBorder = 9.33f;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey("w"))
         {
             transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (transform.position.x > xBorder)
+        {
+           new Vector3(xBorder, transform.position.y, transform.position.z);
+            Debug.Log("bawls");
         }
     }
 }
